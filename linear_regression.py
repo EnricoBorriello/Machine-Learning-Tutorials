bf400e5f523fecb646fe752c821d1c6e7f7a8f6e
Enrico Borriello <borriello.enrico@gmail.com>
2020-12-30 12:35:06 -0700

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -2,10 +2,10 @@
 
 import numpy as np
 
-# Normal equation
+# Normal equation (bias column needs to be included)
 def best_fit_parameters(X,Y):
     return np.linalg.inv(X.T.dot(X)).dot(X.T).dot(Y) 
 
-# Cost function
+# Objective function
 def objective_function(X,Y,theta):
     return (X.dot(theta)-Y).T.dot(X.dot(theta)-Y)[0][0]/2./len(Y)
